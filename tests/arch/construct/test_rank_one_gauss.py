add tests for diag-rank-one kl; they don't pass -> fix!

tests/arch/construct/test_rank_one_gauss.py
@@ -1,10 +1,3 @@
-import os, sys
-sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '../')))
-theano_flags = os.environ['THEANO_FLAGS'] if 'THEANO_FLAGS' in os.environ else ''
-os.environ['THEANO_FLAGS'] = 'device=cpu,' + theano_flags
-os.environ['GNUMPY_IMPLICIT_CONVERSION'] = 'allow'
-
-
 import theano
 import theano.tensor as T
 from breze.arch.construct.layer.distributions import RankOneGauss
@@ -15,15 +8,12 @@ from scipy.stats import multivariate_normal
 # np.set_printoptions(precision=4, linewidth=120)
 
 
-class TestRankOneGauss():
+class TestRankOneGauss(object):
 
     @classmethod
     def setup_class(cls):
-        m, v, u = (T.tensor3(i) for i in ('mean', 'var', 'u'))
-        cls.mean_T = m
-        cls.var_T = v
-        cls.u_T = u
-        cls.gaus = RankOneGauss(m, v, u, eps=0)
+        cls.mean_T, cls.var_T, cls.u_T = (T.tensor3(i) for i in ('mean', 'var', 'u'))
+        cls.gaus = RankOneGauss(cls.mean_T, cls.var_T, cls.u_T, eps=0)
         cls.n = int(1e4)
         cls.dims = 2
         cls.shape = [cls.dims] * 3
@@ -44,24 +34,63 @@ class TestRankOneGauss():
         u = self.u.reshape(-1, 1)
         return np.diagflat(self.var) + u.dot(u.T)
 
-    # def test_sample(self):
-    #     foo = theano.function([self.mean_T, self.var_T, self.u_T], self.gaus.sample())
-    #     X = np.zeros((self.dims ** 3, self.n), dtype=self.floatx)
-    #     for i in xrange(self.n):
-    #         X[:, i] = foo(self.mean, self.var, self.u).flatten()
-    #
-    #     mean = X.mean(axis=1).reshape(self.mean.shape)
-    #     cov = np.cov(X)
-    #     expected_cov = self.cov()
-    #
-    #     assert_allclose(mean, self.mean, 0.05)
-    #     for i in xrange(4):
-    #         c = cov[i*2:(i+1)*2, i*2:(i+1)*2]
-    #         ec = expected_cov[i*2:(i+1)*2, i*2:(i+1)*2]
-    #
-    #         print c
-    #         print ec
-    #         assert_allclose(c, ec, 0.05)
+    def test_time_cov_factor(self):
+        inpts = [T.vector(i) for i in 'var u x'.split()]
+        foo = theano.function(inpts, self.gaus._times_cov_factor(*inpts))
+
+        for i in xrange(self.shape[0]):
+            for j in xrange(self.shape[1]):
+
+                u = self.u[i, j, :]
+                v = self.var[i, j, :]
+                v = np.ones(v.shape, dtype=self.floatx)
+                u = np.ones(u.shape, dtype=self.floatx)
+                x = np.random.randn(self.dims).astype(self.floatx)
+
+                result = (foo(v, u, x) ** 2).sum()
+                x = x.reshape(1, self.dims)
+                u = u.reshape(1, self.dims)
+                cov = u.T.dot(u) + np.diagflat(v)
+                expected = x.dot(cov).dot(x.T)
+                print cov
+                print result, expected
+                assert_allclose(result, expected, 1e-6)
+
+    def test_cov_factor(self):
+        inpts = [T.vector(i) for i in 'var u'.split()]
+        foo = theano.function(inpts, self.gaus._cov_factor(*inpts))
+
+        for i in xrange(self.shape[0]):
+            for j in xrange(self.shape[1]):
+
+                u = self.u[i, j, :]
+                v = self.var[i, j, :]
+
+                result = foo(v, u).reshape(2, 2)
+                result = result.T.dot(result)
+                u = u.reshape(self.dims, 1)
+                cov = u.dot(u.T) + np.diagflat(v)
+                assert_allclose(result, cov, 1e-6)
+
+    def test_sample(self):
+        foo = theano.function([self.mean_T, self.var_T, self.u_T], self.gaus.sample(), on_unused_input='warn')
+        X = np.zeros((self.dims ** 3, self.n), dtype=self.floatx)
+        for i in xrange(self.n):
+            X[:, i] = foo(self.mean, self.var, self.u).flatten()
+
+        mean = X.mean(axis=1).reshape(self.mean.shape)
+        expected_cov = self.cov()
+
+        assert_allclose(mean, self.mean, 0.05)
+        print 'mean =', mean
+        X -= mean.reshape(-1, 1)
+        for i in xrange(4):
+            c = np.cov(X[i*2:(i+1)*2, :])
+            ec = expected_cov[i*2:(i+1)*2, i*2:(i+1)*2]
+
+            print c
+            print ec
+            assert_allclose(c, ec, 0.2)
 
     def test_nll(self):
 